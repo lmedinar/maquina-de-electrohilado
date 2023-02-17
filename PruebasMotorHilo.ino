238d17a870cb4245a78d69d773085fd5c4647864
L. Medina <lmedinar1503@gmail.com>
2023-02-17 13:51:58 -0600

    Cambié un pin y modifiqué para 90 vueltas

--- PruebasMotorHilo.ino
+++ PruebasMotorHilo.ino
@@ -1,3 +1,4 @@
+#define PIN_A4988_VCC 4
 #define PIN_RELES_ALIMENTACION 2
 #define PIN_RELE_MOTOR_A 3
 #define PIN_DIR_A 9
@@ -24,8 +25,10 @@ void setup() {
   pinMode(PIN_RELE_MOTOR_A, OUTPUT);
   pinMode(PIN_ALIM_SWITCH_A, OUTPUT);
   pinMode(PIN_DIR_A, OUTPUT);
+  pinMode(PIN_A4988_VCC, OUTPUT);
   digitalWrite(PIN_RELES_ALIMENTACION, HIGH); // Encendemos motor A
   digitalWrite(PIN_ALIM_SWITCH_A, HIGH); // Alimentamos el switch A
+  digitalWrite(PIN_A4988_VCC, HIGH); // Alimentamos el módulo A4988
   // NOTA: Los relevadores de los motores se encienden en bajo
   analogWrite(PIN_RELE_MOTOR_A, 0); // Iniciamos con motor encendido
   // INICALIZAMOS POSICIÓN HASTA PEGAR CON EL SWITCH
@@ -40,11 +43,11 @@ void loop() {
   // Para dar 90 vueltas necesitamos 200 x 90 pasos
   // Es decir 18000 pasos
   analogWrite(PIN_DIR_A, ARRIBA); // Empezamos moviéndonos hacia arriba
-  for (int i = 0; i < 1800; i++) {
+  for (int i = 0; i < 18000; i++) {
     paso_motor_A(10);
   }
   analogWrite(PIN_DIR_A, ABAJO); // Ahora nos movemos hacia abajo
-  for (int i = 0; i < 1800; i++) {
+  for (int i = 0; i < 18000; i++) {
     // Por seguridad, en cada paso revisamos si no chocamos con el switch
     if (digitalRead(PIN_READ_SWITCH_A)){
         break; // Si chocamos con el switch nos cambiamos a dar 90 vueltas hacia arriba
