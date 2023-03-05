1315e3d7c1dc9a6600ead7702816420be0413d31
Luis A. Medina <lmedinar1503@alumno.ipn.mx>
2023-03-04 22:13:34 -0600

    Se realizaron algunas pruebas de velocidad y se introdujeron algunas líneas para explorar la aceleración del motor principal

--- PruebasMotorHilo.ino
+++ PruebasMotorHilo.ino
@@ -7,15 +7,17 @@
 #define PIN_READ_SWITCH_A 7
 #define ARRIBA 255
 #define ABAJO 0
+#define ESPERA_GEN 1000000
+#define ESPERA_PULSO 9000
 
 int direcion_A;
 
 // FUNCIONES
 void paso_motor_A(int retraso) {
   analogWrite(PIN_STEP_A, 255);
-  delay(2); //
+  delayMicroseconds(ESPERA_PULSO); //
   analogWrite(PIN_STEP_A, 0);
-  delay(retraso);
+  delayMicroseconds(retraso);
 }
 
 
@@ -34,7 +36,7 @@ void setup() {
   // INICALIZAMOS POSICIÓN HASTA PEGAR CON EL SWITCH
   while (!digitalRead(PIN_READ_SWITCH_A)) {
     analogWrite(PIN_DIR_A, ABAJO); // Nos movemos hasta llegar al switch de final de carrera
-    paso_motor_A(20); // Tomamos 10 ms por paso
+    paso_motor_A(10); // Tomamos 10 ms por paso
   }
 }
 
@@ -43,17 +45,25 @@ void loop() {
   // Para dar 90 vueltas necesitamos 200 x 90 pasos
   // Es decir 18000 pasos
   analogWrite(PIN_DIR_A, ARRIBA); // Empezamos moviéndonos hacia arriba
+  // aceleramos
+  
+  // Mantenemos velocidad
   for (int i = 0; i < 18000; i++) {
-    paso_motor_A(10);
+    paso_motor_A(ESPERA_GEN);
   }
+  delay(10);
+
+  
   analogWrite(PIN_DIR_A, ABAJO); // Ahora nos movemos hacia abajo
+  // aceleramos
+  // mantenemos la velocidad
   for (int i = 0; i < 18000; i++) {
     // Por seguridad, en cada paso revisamos si no chocamos con el switch
     if (digitalRead(PIN_READ_SWITCH_A)){
         break; // Si chocamos con el switch nos cambiamos a dar 90 vueltas hacia arriba
       }
-    paso_motor_A(10);
+    paso_motor_A(ESPERA_GEN);
   }
-
+  delay(10);
 
 }
