1cfbbd76a598378b9e985b75009be7b88e6bc8d2
Luis A. Medina <lmedinar1503@alumno.ipn.mx>
2023-05-17 17:45:18 -0600

    Reconfigured everything to work with the Arduino UNO for testing purposes.

--- PruebasMotorHilo/PruebasMotorHilo.ino
+++ PruebasMotorHilo/PruebasMotorHilo.ino
@@ -1,10 +1,10 @@
-#define PIN_RELE_MOTOR_A 53
-#define PIN_DIR_A 49
-#define PIN_STEP_A 51
-#define PIN_ALIM_SWITCH_A 33
-#define PIN_READ_SWITCH_A 31
-#define ARRIBA HIGH
-#define ABAJO LOW
+#define PIN_RELE_MOTOR_A 13
+#define PIN_DIR_A 3
+#define PIN_STEP_A 11
+#define PIN_ALIM_SWITCH_A 10
+#define PIN_READ_SWITCH_A 9
+#define ARRIBA 255
+#define ABAJO 0
 unsigned long ESPERA_GEN = 4000000;
 unsigned long ESPERA_PULSO = 4000000;
 unsigned long PULSO_CALIBRACION = 9000000;
@@ -24,14 +24,15 @@ void setup() {
   Serial.begin(9600);
   pinMode(PIN_RELE_MOTOR_A, OUTPUT);
   pinMode(PIN_ALIM_SWITCH_A, OUTPUT);
-  pinMode(PIN_DIR_A, OUTPUT);
+  //pinMode(PIN_DIR_A, OUTPUT);
   pinMode(PIN_STEP_A, OUTPUT);
   pinMode(PIN_READ_SWITCH_A, INPUT);
   digitalWrite(PIN_ALIM_SWITCH_A, HIGH); // Alimentamos el switch A
   // NOTA: Los relevadores de los motores se encienden en bajo
   digitalWrite(PIN_RELE_MOTOR_A, LOW); // Iniciamos con motor encendido
   // INICALIZAMOS POSICIÓN HASTA PEGAR CON EL SWITCH
-  digitalWrite(PIN_DIR_A, LOW); // Nos movemos hasta llegar al switch de final de carrera
+  // digitalWrite(PIN_DIR_A, LOW); // Nos movemos hasta llegar al switch de final de carrera
+  analogWrite(PIN_DIR_A, ABAJO); // Nos movemos hasta llegar al switch de final de carrera
   while (!digitalRead(PIN_READ_SWITCH_A)) {
     paso_motor_A(PULSO_CALIBRACION); // Tomamos 10 ms por paso
   }
@@ -41,7 +42,8 @@ void loop() {
   // 200 pasos equivalen a una vuelta del motor
   // Para dar 90 vueltas necesitamos 200 x 90 pasos
   // Es decir 18000 pasos
-  digitalWrite(PIN_DIR_A, HIGH); // Empezamos moviéndonos hacia arriba
+  //digitalWrite(PIN_DIR_A, HIGH); // Empezamos moviéndonos hacia arriba
+  analogWrite(PIN_DIR_A, ARRIBA); // Empezamos moviéndonos hacia arriba
   // aceleramos
   
   // Mantenemos velocidad
@@ -51,7 +53,8 @@ void loop() {
   delay(10);
 
   
-  digitalWrite(PIN_DIR_A, LOW); // Ahora nos movemos hacia abajo
+  //digitalWrite(PIN_DIR_A, LOW); // Ahora nos movemos hacia abajo
+  analogWrite(PIN_DIR_A, ABAJO); // Ahora nos movemos hacia abajo
   // aceleramos
   // mantenemos la velocidad
   for (int i = 0; i < 18000; i++) {
